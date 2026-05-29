b1e696eb49c7099e980d2ed553041fd927817d71
Aashif Sajah <masaashifsajah@gmail.com>
2026-05-29 16:59:25 +0530

    Ignore and remove .netlify build output from repo

--- .netlify/functions-internal/server/_tanstack-start-manifest_v-eLAwN1cy.mjs
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "/Users/aashif/Desktop/kaero-magic-capture/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index-D2HZKf5q.js"], scripts: [{ attrs: { type: "module", async: true, src: "/assets/index-D2HZKf5q.js" } }] }, "/": { filePath: "/Users/aashif/Desktop/kaero-magic-capture/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-B73otcg2.js"] } } });
-export {
-  tsrStartManifest
-};
