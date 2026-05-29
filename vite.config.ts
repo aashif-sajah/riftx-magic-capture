a20e6cd8d55e950bc20c1a296ed8212d5ae35133
Aashif Sajah <masaashifsajah@gmail.com>
2026-05-29 18:05:25 +0530

    Support dual deployment (Netlify and Vercel) by relying on Nitro auto-detection

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     tailwindcss(),
     tanstackStart({ server: { entry: "server" } }),
     react(),
-    nitro({ preset: "netlify" }),
+    nitro(),
     tsconfigPaths(),
   ],
 });
